dd46dc605cdd2f4c9e56752525f55b714afddd45
Alejandro Velasco <savelascod@unal.edu.co>
2015-09-02 17:43:07 -0500

    -Starting Another Activity (Android Tutorial)

--- app/src/main/java/com/mordreth/app/myhelloapp/app/MainActivity.java
+++ app/src/main/java/com/mordreth/app/myhelloapp/app/MainActivity.java
@@ -1,17 +1,30 @@
 package com.mordreth.app.myhelloapp.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static final String EXTRA_MESSAGE = "com.mordreth.myhelloapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
 
